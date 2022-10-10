8e0d7e11d6df1558e8b0d6c064fc9eb47eb34d30
Logan CoBell <lcobell@cps.edu>
2022-10-10 20:39:13 +0000

    Some formula stuff from Jelani's presentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,28 @@ class ApplicationController < ActionController::Base
     @square_of_num = @num ** 2
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
+  def payment_calculation
+    apr = params.fetch("apr")
+    number_of_years = params.fetch("number_of_years")
+    principal = params.fetch("principal")
+
+    n = number_of_years.to_i * 12
+    apr = apr.to_f / 100
+    r = apr / 12
+
+    numerator = r * principal.to_f
+    denominator = 1 -(1 + r)**(-n)
+
+   @monthly_payment = numerator / denominator
+
+   render({ :template})
+  end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render ({ :template => "calculation_templates/random_results.html.erb"})
+  end
 end
